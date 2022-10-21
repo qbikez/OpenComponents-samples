515c66e38908dc35b7b99274b45bc89c07dec6b1
jakub.pawlowski <jakub.pawlowski@guestline.com>
2022-10-21 13:39:46 +0200

    webshop initial commit

---
+++ webshop/product/product-page/server.js
@@ -0,0 +1,15 @@
+import * as db from './products-db.json';
+
+export const data = (context, callback) => {
+  const { productId } = context.params;
+  const { staticPath } = context;
+
+  const product = db.products[productId];
+
+  callback(null, {
+    productId,
+    product,
+    staticPath,
+    baseUrl: context.baseUrl
+  });
+};
